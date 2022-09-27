99186d6c7856036ca6e4570ebf3d28bebe4650bb
Yurii-Polischukkk <113755012+Yurii-Polischukkk@users.noreply.github.com>
2022-09-27 22:35:11 +0300

    last

--- lab/Lab2 2d/Assets/Scripts/PlayerMovement.cs
+++ lab/Lab2 2d/Assets/Scripts/PlayerMovement.cs
@@ -7,38 +7,34 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 5f;
     public float jumpHeight = 5f;
-    CapsuleCollider2D capsuleCollider;
 
     Vector2 movementVector;
     Rigidbody2D rbody;
+    // Start is called before the first frame update
     void Start()
     {
         rbody = GetComponent<Rigidbody2D>();
-        capsuleCollider = GetComponent<CapsuleCollider2D>();
     }
 
-
+    // Update is called once per frame
     void Update()
     {
-        Vector2 playerVelocity = new Vector2(movementVector.x * movementSpeed, rbody.velocity.y);
-        rbody.velocity = playerVelocity;
+        Vector2 plaverVelocity = new Vector2(movementVector.x * movementSpeed, rbody.velocity.y);
+        rbody.velocity = plaverVelocity;
     }
 
     private void OnMove(InputValue value)
     {
         movementVector = value.Get<Vector2>();
-
+        Debug.Log(movementVector);
     }
 
     private void OnJump(InputValue value)
     {
-        if (!capsuleCollider.IsTouchingLayers(LayerMask.GetMask("Ground")))
-        {
-            return;
-        }
         if (value.isPressed)
         {
             rbody.velocity += new Vector2(0f, jumpHeight);
         }
     }
+
 }
